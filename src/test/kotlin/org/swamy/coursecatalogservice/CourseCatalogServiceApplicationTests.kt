7966ec63a4d59d199dc14e8cd1ff9d81b1d4b1a3
Swamy Sivasubramaniyan <s.swamynathan@gmail.com>
2022-12-27 18:02:42 -0700

    real app, coming up!

---
+++ src/test/kotlin/org/swamy/coursecatalogservice/CourseCatalogServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.swamy.coursecatalogservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseCatalogServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

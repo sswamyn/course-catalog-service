08f80e43598d3b7eed8b536f812e1ec508d16635
Swamy Sivasubramaniyan <s.swamynathan@gmail.com>
2022-12-27 19:07:05 -0700

    Adding the Kotlin Logger

--- src/main/kotlin/org/swamy/coursecatalogservice/controller/GreetingController.kt
+++ src/main/kotlin/org/swamy/coursecatalogservice/controller/GreetingController.kt
@@ -1,5 +1,6 @@
 package org.swamy.coursecatalogservice.controller
 
+import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,6 +11,7 @@ import org.swamy.coursecatalogservice.service.GreetingService
 @RequestMapping("/v1/greetings")
 class GreetingController(val greetingService: GreetingService) {
 
+    companion object : KLogging()
     @GetMapping("/{name}")
     fun retrieveGreeting(@PathVariable("name") name: String): String {
 
@@ -17,6 +19,7 @@ class GreetingController(val greetingService: GreetingService) {
         // Instead of keeping the logic to build the response in the Controller,
         //      we will move it to the Service class service/GreetingService
         //      this is accomplished by injecting that Service into the constructor of the controller
+        logger.info("Name is $name")
         return greetingService.retrieveGreeting(name)
     }
 }
